2de6132fe91feebbdff12d9f26db961121d68306
zhangdongmeng <64208692+zhangdongmeng@users.noreply.github.com>
2020-04-25 19:22:09 -0400

    sub demo

---
+++ pfgd4/Assets/Scripts/CameraFlow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFlow : MonoBehaviour
+{
+    public GameObject bow;               //follow the bow
+    public float smothing = 5f;          
+    Vector3 offset;                      
+
+    void Start()
+    {
+        offset = transform.position - bow.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 target = bow.transform.position + offset;
+        //smooth the camera
+        transform.position = Vector3.Lerp(transform.position, target, smothing * Time.deltaTime);
+    }
+}
